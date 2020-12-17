f3976677c71c259dda92115fcdb7be1b6acb59b3
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-12-17 19:25:56 +0200

    testing ci cd

--- shopping-tracker/src/styles/styledComponents/styledNavBar.js
+++ shopping-tracker/src/styles/styledComponents/styledNavBar.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import ToolBar from "@material-ui/core/ToolBar";
+import { ToolBar } from "@material-ui/core";
 
 export const StyledLink = styled(Link)`
   color: ${(props) => (props.color ? props.color : "white")};
