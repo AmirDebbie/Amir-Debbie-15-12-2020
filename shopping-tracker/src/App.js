f8fade11270364e04690b90306ebc9b83cc1ad97
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-12-31 19:17:04 +0200

    Update App.js

--- shopping-tracker/src/App.js
+++ shopping-tracker/src/App.js
@@ -13,8 +13,6 @@ import { ThemeProvider } from "styled-components";
 import { getTheme, GlobalStyle } from "./styles";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { CURRENCY_API_URL, CURRENCY_INTERVAL_TIME } from "./helpers";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 function App() {
   const dispatch = useDispatch();
@@ -47,10 +45,6 @@ function App() {
       window.removeEventListener("resize", setInnerWidthEventListener);
     };
   }, [fetchCurrency, dispatch, setInnerWidthEventListener]);
-
-  useEffect(() => {
-    AOS.init({ duration: 700 });
-  }, []);
 
   return (
     <HashRouter>
